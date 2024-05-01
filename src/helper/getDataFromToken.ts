195714ba15b10ce7caab4ecda27022c0e8046c9a
Pritam <pritamjana360@gmail.com>
2024-05-01 17:13:26 +0530

    V-1.2.3
    Create fetchadmin token function

--- src/helper/getDataFromToken.ts
+++ src/helper/getDataFromToken.ts
@@ -1,18 +1,24 @@
-import { NextRequest } from "next/server";
-import  jwt  from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import { HttpStatusCode } from "axios";
 
-export const getDataFromToken = (request:NextRequest)=>{
+export const getDataFromToken = (request: NextRequest) => {
 
+    const token = String(request.cookies.get("admintoken")?.value);
+
+    if (!token) {
+        return new NextResponse(JSON.stringify({ error: "Token not found" }), { status: HttpStatusCode.Unauthorized, headers: { 'Content-Type': 'application/json' } });
+    }
     try {
-        
-       const token =  request.cookies.get("authtoken")?.value || " ";
-      const decodeToken:any =  jwt.verify(token, process.env.JWT_SECRETE_KEY!);
 
-      return decodeToken.user.id
+        //    const token =  String(request.cookies.get("authtoken")?.value);
+        const decodeToken: any = jwt.verify(token, process.env.JWT_SECRETE_KEY!);
 
-    } catch (error:any) {
+        return decodeToken.user.id
+
+    } catch (error: any) {
         throw new Error(error.message);
         // throw new Error("Error");
-        
+
     }
 }
